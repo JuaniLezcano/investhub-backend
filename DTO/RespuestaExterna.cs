faa488f8394c2f28d619c80f93f4e28e16d43aa0
Valentin <peresvalentin09@gmail.com>
2024-09-11 20:07:45 -0300

    [feat]: DTO

---
+++ DTO/RespuestaExterna.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace investhub_backend.DTO
+{
+    public class RespuestaExterna<T>
+    {
+        public bool Exito { get; set; } = false; //Sirve como bandera para determinar si la operacion fue exitosa o no
+        public string MensajePublico { get; set; } = ""; // Da retroalimentacion del error
+        public T Datos { get; set; } // Un tipo de objeto o coleccion, contiene los datos de la respuesta ( que pueden ser de cualquier tipo)
+        public override string ToString() //Aca utilizamos un metodo que convierte al objeto en un JSON, esto es util para enviar a APIs o al que tenga que utilizarlo
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
+
+/*
+Este seria un ejemplo de uso del DTO que podriamos devolver como JSON a una API
+var respuesta = new RespuestaExterna<string>()
+{
+    Exito = true,
+    MensajePublico = "Operación exitosa",
+    Datos = "Datos de cliente"
+};
+
+*/
---
+++ DTO/RespuestaInterna.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace investhub_backend.DTO
+{
+    public class RespuestaInterna<T> //T es un tipo de dato generico, puede ser cualquier tipo de dato o una coleccion
+    {
+        public T Datos { get; set; }
+        public bool Exito { get; set; } = false;
+        public string Mensaje { get; set; } = "";
+    }
+}
+
+//Mismo funcionamiento que respuesta externa pero sin usar el JSON
