696ab690b20b3f9903887729155b9ef99c0444bf
Valentin Peres <peresvalentin09@gmail.com>
2024-09-13 19:48:35 -0300

    [feat]: Migracion inicial

---
+++ Migrations/20240913210521_InitialCreate.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace investhub_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Email",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Direccion = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Email", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Portafolio",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portafolio", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Accion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Cantidad = table.Column<int>(type: "integer", nullable: false),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    PrecioActual = table.Column<float>(type: "real", nullable: false),
+                    PrecioCompra = table.Column<float>(type: "real", nullable: false),
+                    Simbolo = table.Column<string>(type: "text", nullable: false),
+                    TipoAccion = table.Column<int>(type: "integer", nullable: false),
+                    PortafolioId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accion", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Accion_Portafolio_PortafolioId",
+                        column: x => x.PortafolioId,
+                        principalTable: "Portafolio",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ahorro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Cantidad = table.Column<float>(type: "real", nullable: false),
+                    tipoMoneda = table.Column<int>(type: "integer", nullable: false),
+                    PortafolioId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ahorro", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ahorro_Portafolio_PortafolioId",
+                        column: x => x.PortafolioId,
+                        principalTable: "Portafolio",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Usuario = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    EmailId = table.Column<int>(type: "integer", nullable: false),
+                    PortafolioId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cliente_Email_EmailId",
+                        column: x => x.EmailId,
+                        principalTable: "Email",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Cliente_Portafolio_PortafolioId",
+                        column: x => x.PortafolioId,
+                        principalTable: "Portafolio",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accion_PortafolioId",
+                table: "Accion",
+                column: "PortafolioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ahorro_PortafolioId",
+                table: "Ahorro",
+                column: "PortafolioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cliente_EmailId",
+                table: "Cliente",
+                column: "EmailId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cliente_PortafolioId",
+                table: "Cliente",
+                column: "PortafolioId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accion");
+
+            migrationBuilder.DropTable(
+                name: "Ahorro");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+
+            migrationBuilder.DropTable(
+                name: "Email");
+
+            migrationBuilder.DropTable(
+                name: "Portafolio");
+        }
+    }
+}
