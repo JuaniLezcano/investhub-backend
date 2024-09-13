343dd28060a64056e66539764129c03d959ffdc6
Valentin Peres <peresvalentin09@gmail.com>
2024-09-13 19:51:06 -0300

    [refactor]: Reescribo controlador para usar los servicios

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,4 +1,6 @@
 using investhub_backend.Data.Models;
+using investhub_backend.DTO;
+using investhub_backend.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
@@ -9,70 +11,68 @@ namespace investhub_backend.Controllers
     [Route("[controller]")] //Define la ruta donde llegaran las solicitudes, en este caso se reemplaza automaticamente con /Cliente
     public class ClienteController : ControllerBase
     {
-        private readonly ILogger<ClienteController> _logger; //logger es el registro de mensajes
-        private static List<Cliente> clientes = new List<Cliente>(); //Por el momento esto es una lista estatica, al tener una bd deberiamos de reemplazarlo
+		private readonly ILogger<ClienteController> _logger;  //logger es el registro de mensajes
+		private readonly IClienteServicio _clienteServicio;
 
-        public ClienteController(ILogger<ClienteController> logger)
-        {
-            _logger = logger; //Inicializa el registro de eventos y errores 
+		public ClienteController(IClienteServicio clienteServicio, ILogger<ClienteController> logger)
+		{
+			_clienteServicio = clienteServicio;
+			_logger = logger; //Inicializa el registro de eventos y errores 
         }
 
-        // Obtener todos los clientes
-        [HttpGet]
-        public ActionResult<IEnumerable<Cliente>> GetClientes() //Los IEnumerables son tipos de colecciones ( En este caso le estoy pidiendo que me devuelva una coleccion de clientes)
-        {
-            return Ok(clientes);
-        }
+		// Obtener todos los clientes
+		[HttpGet(Name = "GetClientes")]
+		public async Task<ActionResult<RespuestaExterna<List<Cliente>>>> Get()
+		{
+			var respuesta = new RespuestaExterna<List<Cliente>>();
+			try
+			{
+				var respuestaInterna = await _clienteServicio.ObtenerAsync();
+				if (respuestaInterna.Exito)
+				{
+					respuesta.Exito = true;
+					respuesta.MensajePublico = "Clientes recuperados correctamente";
+					respuesta.Datos = respuestaInterna.Datos;
+					return respuesta;
+				}
+				else
+				{
+					respuesta.MensajePublico = "Hubo un error al recuperar los clientes";
+					return BadRequest(respuesta);
+				}
+			}
+			catch (Exception ex)
+			{
+				respuesta.MensajePublico = "Hubo un error al recuperar los clientes";
+				return StatusCode(StatusCodes.Status500InternalServerError, respuesta);
+			}
+		}
 
-        // Obtener un cliente por Id
-        [HttpGet("{id}")]
-        public ActionResult<Cliente> GetCliente(int id)
-        {
-            var cliente = clientes.FirstOrDefault(c => c.Id == id); //Hago una consulta para encontrar el cliente 
-            if (cliente == null)
+		// Agregar un nuevo cliente
+		[HttpPost(Name = "PostCliente")]
+		public async Task<ActionResult<RespuestaExterna<bool>>> Post(Cliente cliente)
+		{
+            var respuesta = new RespuestaExterna<bool>();
+            try
             {
-                return NotFound();
+                var respuestaInterna = await _clienteServicio.AgregarAsync(cliente);
+                if (respuestaInterna.Exito)
+                {
+                    respuesta.Exito = true;
+                    respuesta.Datos = true;
+                    return StatusCode(StatusCodes.Status201Created, respuesta);
+                }
+                else
+                {
+                    respuesta.MensajePublico = "Intente colocar otro CUIT.";
+                    return BadRequest(respuesta);
+                }
             }
-            return Ok(cliente);
-        }
-
-        // Agregar un nuevo cliente
-        [HttpPost]
-        public ActionResult<Cliente> PostCliente(Cliente cliente)
-        {
-            cliente.Id = clientes.Count > 0 ? clientes.Max(c => c.Id) + 1 : 1; // Generar un Id, si ya existe el cliente toma el id mas grande y le suma 1
-            clientes.Add(cliente);
-            return CreatedAtAction(nameof(GetCliente), new { id = cliente.Id }, cliente);
-        }
-
-        // Actualizar un cliente existente
-        [HttpPut("{id}")]
-        public IActionResult PutCliente(int id, Cliente updatedCliente)
-        {
-            var cliente = clientes.FirstOrDefault(c => c.Id == id);
-            if (cliente == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                respuesta.MensajePublico = "Ocurrio un error al agregar al Cliente.";
+                return StatusCode(StatusCodes.Status500InternalServerError, respuesta);
             }
-
-            cliente.Usuario = updatedCliente.Usuario;
-            cliente.Password = updatedCliente.Password;
-
-            return NoContent();
-        }
-
-        // Eliminar un cliente
-        [HttpDelete("{id}")]
-        public IActionResult DeleteCliente(int id)
-        {
-            var cliente = clientes.FirstOrDefault(c => c.Id == id);
-            if (cliente == null)
-            {
-                return NotFound();
-            }
-
-            clientes.Remove(cliente);
-            return NoContent();
         }
     }
 }
